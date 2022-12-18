621e6b23b2bb04d99c9a8b482f0a3e42b1aadbbd
AbdullahASCS <abdullahsulcs@gmail.com>
2022-12-18 17:30:58 +0300

    first

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -14,15 +14,23 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
 
-// example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
+app.get('/rovers/:selectedRover',async (req, res) =>{
+   let {selectedRover} = req.params
+    try{
+        const max_sol = await fetch(
+            `https://api.nasa.gov/mars-photos/api/v1/manifests/${selectedRover}/?API_KEY=${process.env.API_KEY}`
+          )
+            .then((res) => res.json())
+            .then((res) => res.photo_manifest.max_sol);
 
+        const images = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${selectedRover}/photos?sol=${max_sol}&api_key=${process.env.API_KEY}`
+        ).then((res)=> res.json());
+        res.send(images);
+    }
+    catch (err){
+        console.log(err);
+    }
+}
+
+)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
